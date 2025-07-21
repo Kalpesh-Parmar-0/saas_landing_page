ebbd1d7d0af1c1913beb83b66510352c91d02e9c
Kalpesh-Parmar-0 <kalpesh2112004@gmail.com>
2025-07-21 07:33:43 +0530

    features  done

---
+++ src/sections/Features.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Element } from "react-scroll";
+import { features } from "../constans";
+
+const Features = () => {
+  return (
+    <section>
+      <Element name="features">
+        <div className="container">
+          <div className="relative flex md:flex-wrap flex-nowrap border-2 border-s3 rounded-7xl md:overflow-hidden max-md:flex-col feature-after md:g7 max-md:border-none max-md:rounded-none max-md:gap-3">
+            {features.map(({id, icon, caption, title, text, button}) => (
+              <div
+                key={id}
+                className="relative z-2 ma:px-10 px-5 md:pb-10 pb-5 flex-50 max-md:g7 max-md:border-2 max-md:border-s3 max-md:rounded-3xl max-md:flex-320"
+              >
+                <div className="w-full flex justify-start items-start">
+                  <div className="">
+                    <img src={icon} alt={title} className="size-28 object-contain"/>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </Element>
+    </section>
+  );
+};
+
+export default Features;
